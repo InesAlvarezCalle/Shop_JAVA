35e7dbde9f47961130a8a328522c5bfec9984143
InesAlvarezCalle <inesalvarezdaw@gmail.com>
2025-02-24 08:32:58 +0100

    Empezamos proyecto Tienda y creamos la clase Articulo con sus atributos

---
+++ Articulo.java
@@ -0,0 +1,57 @@
+public class Articulo {
+    private String codigo;
+    private double precio;
+    private int iva;
+    private int stock;
+
+    //Constructor por defecto
+    public Articulo(){
+        this.codigo = "";
+        this.precio = 0.0;
+        this.iva = 0;
+        this.stock = 0;
+    }
+
+    //Contructor general
+    public Articulo(String cod, double pre, int iva, int stc){
+        this.codigo = cod;
+        this.precio = pre;
+        this.iva = iva;
+        this.stock = stc;
+    }
+
+    //getters and setters
+    public String getCodigo(){
+        return this.codigo;
+    }
+
+    public void setCodigo(String cod) {
+        this.codigo = cod;
+    }
+
+    public double getPrecio(){
+        return this.precio;
+    }
+
+    public void setPrecio(double pre){
+        this.precio = pre;
+    }
+
+    public int getIva(){
+        return this.iva;
+    }
+
+    public void setIva(int iva){
+        this.iva = iva;
+    }
+
+    public int getStock(){
+        return this.stock;
+    }
+
+    public void setStock(int stc){
+        this.stock = stc;
+    }
+
+
+}
