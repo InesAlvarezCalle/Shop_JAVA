25c4c88c47de495aa28de490de0d8f8a08bac06d
InesAlvarezCalle <inesalvarezdaw@gmail.com>
2025-02-24 08:46:54 +0100

    Comenzamos clase Oficina con sus atributos

--- Oficina.java
+++ Oficina.java
@@ -1,3 +1,24 @@
 public class Oficina extends Articulo{
+    private String descripcion;
 
+    //Contructor por defecto
+    public Oficina(){
+        super();
+        this.descripcion = "";
+    }
+
+    //Constructor general
+    public Oficina(String cod, double pre, int iva, int stc, String des){
+        super(cod, pre, iva, stc);
+        this.descripcion = des;
+    }
+
+    //getters y setters
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String des){
+        this.descripcion = des;
+    }
 }
